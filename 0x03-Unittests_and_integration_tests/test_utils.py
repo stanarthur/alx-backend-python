83442ddb21de0cc5998063f1198a97b37cbdeaae
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-05 19:20:38 +0100

    0. Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,21 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import Tuple, Dict, Union
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """To Test access nested map"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple,
+            expected_answer: Union[int, Dict],
+            ) -> None:
+        """This tests access_nested_map output"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_answer)
