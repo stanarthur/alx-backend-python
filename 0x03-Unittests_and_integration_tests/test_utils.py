9caaef5447b368bb85a5bb3721496e15e6af1192
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2024-01-06 12:20:03 +0100

    1. Checking something

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -24,16 +24,16 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected_answer)
 
-        @parameterized.expand([
-            ({}, ("a",), KeyError),
-            ({"a": 1}, ("a", "b"), KeyError),
-        ])
-        def test_access_nested_map_exception(
-                self,
-                nested_map: Dict,
-                path: Tuple[str],
-                exception: Exception,
-                ) -> None:
-            """To test access_nested_map's exception."""
-            with self.assertRaises(exception):
-                access_nested_map(nested_map, path)
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
